5ea90df6ddd2be3a161c0aa11419e83ee0a17685
Damian Demasi <damian.demasi.1@gmail.com>
2020-05-14 09:13:23 +0930

    Creating a user and a post

--- embed.js
+++ embed.js
@@ -13,5 +13,37 @@ const userSchema = new mongoose.Schema({
 //      Creating the model
 const User = mongoose.model('User', userSchema);
 
-
 // Post model: title, content
+//      Creating the schema
+const postSchema = new mongoose.Schema({
+    title: String,
+    content: String
+});
+//      Creating the model
+const Post = mongoose.model('Post', postSchema);
+
+// // Creating a user
+// const newUser = new User({
+//     email: 'something@else.com',
+//     name: 'Some User Name'
+// });
+// newUser.save((err, user) => {
+//     if(err) {
+//         console.log('ERROR: ' + err);
+//     } else {
+//         console.log('User: ' + user);
+//     }
+// });
+
+// // Creating a post
+// const newPost = new Post({
+//     title: 'This is a great post',
+//     content: 'Awesome post content.'
+// });
+// newPost.save((err, post) => {
+//     if(err) {
+//         console.log('ERROR: ' + err);
+//     } else {
+//         console.log('User: ' + post);
+//     }
+// });
